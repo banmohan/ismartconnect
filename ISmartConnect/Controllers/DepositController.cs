b2d67b9368b39ae7512af61a1dd5ccaee393c1b7
Mohan Ban <banmohan@outlook.com>
2025-12-14 09:48:42 +0545

    iso response code added

--- ISmartConnect/Controllers/DepositController.cs
+++ ISmartConnect/Controllers/DepositController.cs
@@ -13,48 +13,76 @@ public class DepositController(AccountIntercomService accountIntercom) : Control
     public async Task<ActionResult<ResBalance>> Balance(ReqAccount req)
     {
         var res = await accountIntercom.BalanceEnquiryAsync(req);
-        return Ok(res);
+        return Ok(new
+        {
+            res,
+            IsoResponseCode = "00"
+        });
     }
 
     [HttpPost("mini-statement")]
     public async Task<ActionResult<ResMiniStatement>> MiniStatement(ReqAccount req)
     {
         var res = await accountIntercom.MiniStatementAsync(req);
-        return Ok(res);
+        return Ok(new
+        {
+            res,
+            IsoResponseCode = "00"
+        });
     }
 
     [HttpPost("full-statement")]
     public async Task<ActionResult<IEnumerable<StatementItem>>> FullStatement(ReqFullStatement req)
     {
         var res = await accountIntercom.FullStatementAsync(req);
-        return Ok(res);
+        return Ok(new
+        {
+            res,
+            IsoResponseCode = "00"
+        });
     }
 
     [HttpPost("fund-transfer")]
     public async Task<ActionResult<ResFundTransfer>> FundTransfer(ReqFundTransfer req)
     {
         var res = await accountIntercom.FundTransferAsync(req);
-        return Ok(res);
+        return Ok(new
+        {
+            res,
+            IsoResponseCode = "00"
+        });
     }
 
     [HttpPost("reversal")]
     public async Task<ActionResult<bool>> Balance(ResFundTransfer req)
     {
         var res = await accountIntercom.ReversalAsync(req);
-        return Ok(res);
+        return Ok(new
+        {
+            res,
+            IsoResponseCode = "00"
+        });
     }
 
     [HttpPost("acc-validation")]
     public async Task<ActionResult<ResAccountValidation>> AccValidation(ReqAccount req)
     {
         var res = await accountIntercom.AccValidateAsync(req);
-        return Ok(res);
+        return Ok(new
+        {
+            res,
+            IsoResponseCode = "00"
+        });
     }
-    
+
     [HttpGet("all-account/{account}")]
     public async Task<ActionResult<ResAllAccount>> AllAccount(string account)
     {
         var res = await accountIntercom.AllAccountAsync(account);
-        return Ok(res);
+        return Ok(new
+        {
+            res,
+            IsoResponseCode = "00"
+        });
     }
 }
--- ISmartConnect/Controllers/LoanController.cs
+++ ISmartConnect/Controllers/LoanController.cs
@@ -13,20 +13,35 @@ public class LoanController(AccountIntercomService accountIntercom) : Controller
     public async Task<ActionResult<ResLoanDetails>> LoanDetail(ReqLoanDetail req)
     {
         var res = await accountIntercom.LoanDetailAsync(req);
-        return Ok(res);
+                return Ok(new
+        {
+            res,
+            IsoResponseCode = "00"
+        });
+
     }
 
     [HttpPost("loan-statement")]
     public async Task<ActionResult<IEnumerable<ResLoanStatement>>> LoanStatement(ReqLoanStatement req)
     {
         var res = await accountIntercom.LoanStatementAsync(req);
-        return Ok(res);
+                return Ok(new
+        {
+            res,
+            IsoResponseCode = "00"
+        });
+
     }
 
     [HttpPost("loan-schedule")]
     public async Task<ActionResult<IEnumerable<ResLoanSchedule>>> LoanSchedule(ReqAccount req)
     {
         var res = await accountIntercom.LoanScheduleAsync(req);
-        return Ok(res);
+                return Ok(new
+        {
+            res,
+            IsoResponseCode = "00"
+        });
+
     }
 }
